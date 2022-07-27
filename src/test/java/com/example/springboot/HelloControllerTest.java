b9db40a3202fb96de2dbd04f230cb466bc0061cb
assafsauer <as.sauer@gmail.com>
2022-07-27 14:33:36 +0200

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings sauer from Spring Boot  + Tanzu!"));
+            .andExpect(content().string("Greetings DHLaa-build from Spring Boot  + Tanzu!"));
     }
 }
